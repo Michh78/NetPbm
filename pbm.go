e6dc96a0a62ce836f9277676cd8f71159657225b
Michh <michel.guy@ynov.com>
2024-01-09 11:34:35 +0100

    m

---
+++ pbm.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+PBM  struct {
+	data [][] bool 
+   width , height  int 
+   magicNumber  string 
+}
+
+func main() {
+
+}
+
+func  ReadPBM ( chaîne de nom de fichier  ) ( * PBM , erreur ) {
+	
+}
